3f6317a70cd75bcf5b0ba4de71de07696f454a34
Priyanshu Rathore <priyanshu@archPride.localdomain>
2022-11-24 23:40:19 +0530

    Added : UC3

---
+++ FlipCoinSimulationProblem/UC-3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+
+#read -p "Enter number of times a coin should filp : " n;
+
+head=0;
+tail=0;
+
+
+while [ $head -le 21 ] || [ $tail -le 21 ]
+do
+    coin=$((RANDOM%2))
+    if [ $coin -eq 1 ]
+    then
+    # head=$(($head + 1))
+    ((head++))
+else
+    # tail=$(($tail + 1))
+    ((tail++))
+fi
+done
+
+echo "HEAD wins $head times"
+echo "TAIL wins $tail times"
