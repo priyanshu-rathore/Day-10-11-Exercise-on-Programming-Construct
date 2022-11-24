145b745027feb9b0c5164dc710715c29d5e7aff4
Priyanshu Rathore <priyanshu@archPride.localdomain>
2022-11-24 23:04:58 +0530

    Added : UC8

--- SortingArithmeticComputationProblem/UC-8.sh
+++ SortingArithmeticComputationProblem/UC-8.sh
@@ -28,3 +28,15 @@ arr[$i]=${numbers[$i]}
 done
 
 echo ${arr[@]}
+
+for ((i=0;i<${#arr[@]};i++))
+do
+if [ ${arr[$i]} -lt ${arr[$((i+1))]} ]
+then
+temp=${arr[$i]};
+arr[$i]=${arr[$((i+1))]};
+arr[$((i+1))]=$temp;
+fi
+done
+
+echo "sorted array descending order : ${arr[@]}"
